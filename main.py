a313ff66c215edfaace4a8a5e4250d3c8cbb56cf
Miranda Lyng <lyngmiranda@gmail.com>
2026-02-17 13:12:37 +0100

    added rejected and analytics csv - files

--- main.py
+++ main.py
@@ -27,12 +27,8 @@ if __name__ == '__main__':
     # Date to the right format
     products["created_at"] = pd.to_datetime(
         products["created_at"], errors="coerce", format="mixed")
-
-    # -----------------------#
-    ####### REJECT   #######
-    # -----------------------#
-
-    # price that is 0 or negative
+    # price to numeric
+    products["price"] = pd.to_numeric(products["price"], errors="coerce")
 
     # -----------------------#
     ####### FLAGGING   #######
@@ -47,4 +43,48 @@ if __name__ == '__main__':
     # flag for missing price
     products["missing_price"] = products["price"].isna()
 
-    print(products.head(20))
+    products["low_price"] = products["price"] == 0
+
+    products["luxury_products"] = products["price"] > 4000
+    # -----------------------#
+    ####### REJECT   #######
+    # -----------------------#
+
+    rejected_data = (
+        products["id"].isna() |
+        products["price"].isna() |
+        (products["price"] < 0)
+    )
+
+    df_rejected = products[rejected_data].copy()
+    df_valid = products[~rejected_data].copy()
+
+    # Valid csv_file with analytics summary
+
+    mean_price = df_valid["price"].mean()
+    median_price = df_valid["price"].median()
+    num_of_products = len(df_valid)
+    num_of_missing_price = len(df_rejected["price"])
+
+    # create a dataframe for the analytics summary
+
+    analytics_summary = pd.DataFrame({
+        "mean_price": [mean_price],
+        "median_price": [median_price],
+        "num_of_products": [num_of_products],
+        "num_of_missing_price": [num_of_missing_price]
+    })
+
+    # read to a new csv - file
+    analytics_summary.to_csv("analytics_summary.csv", index=False)
+
+    # extract and calculate the top 10 expensive products
+    top_10_products = df_valid.sort_values("price", ascending=False).head(10)
+
+    top_10_invalid_prices = df_rejected.sort_values(
+        "price").head(10)
+
+    print(top_10_invalid_prices)
+
+    # Rejected products in a seperate csv file
+    rejected_products = df_rejected.to_csv("rejected_products.csv")
