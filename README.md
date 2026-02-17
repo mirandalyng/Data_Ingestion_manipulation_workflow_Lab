df6ebc458a4f62c22d652080879f58e56464762b
Miranda Lyng <lyngmiranda@gmail.com>
2026-02-17 12:30:40 +0100

    Added flag and tranformation for the data

--- README.md
+++ README.md
@@ -7,4 +7,21 @@ $ uv init
 $ uv sync
 ```
 
-## 2.
+## Data Quality Checks
+
+**Accept**
+
+- Correct data is accepted.
+
+**Transform**
+
+- Incorrect data in forms of datatype, space e.g is transformed without changing the _meaning_ of the data.
+
+**Flag**
+
+- The data that _could_ be wrong is flagged.
+- It is excludet from the calculations-
+
+**Reject**
+
+- Invalid data (e.g where obligated fields are missing) is rejeted and stored in a file: _rejected_products.csv_
--- main.py
+++ main.py
@@ -1,4 +1,50 @@
 import pandas as pd
 
-# Read the csv-file
-read_products = pd.read_csv("products.csv")
+
+if __name__ == '__main__':
+    # Read the csv-file
+    products = pd.read_csv("products.csv", sep=";")
+
+    # -----------------------#
+    ####### TRANSFORM #######
+    # -----------------------#
+
+    # clean the column names
+    products.columns = products.columns.str.strip()
+    # remove withespace before and after
+    # uppercase in the id
+    # titles in the names
+    # Replacing spaces and -
+
+    products["id"] = products["id"].str.strip().replace(
+        " ", "").replace("_", "-").str.upper()
+
+    products["currency"] = products["currency"].str.strip()
+
+    products["name"] = products["name"].str.strip(
+    ).str.title().replace(r"\s+", " ", regex=True)
+
+    # Date to the right format
+    products["created_at"] = pd.to_datetime(
+        products["created_at"], errors="coerce", format="mixed")
+
+    # -----------------------#
+    ####### REJECT   #######
+    # -----------------------#
+
+    # price that is 0 or negative
+
+    # -----------------------#
+    ####### FLAGGING   #######
+    # -----------------------#
+
+    # flag for missing currency
+    products["missing_currency"] = products["currency"].isna()
+
+    # flag for missing created at
+    products["missing_date"] = products["created_at"].isna()
+
+    # flag for missing price
+    products["missing_price"] = products["price"].isna()
+
+    print(products.head(20))
